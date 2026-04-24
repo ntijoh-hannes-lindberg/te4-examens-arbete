279b8579a84218ce9efebf9899029cf5f1f58cf4
hanneslindberg <hannes.lindberg@elev.ga.ntig.se>
2026-04-24 14:17:14 +0200

    feat: get prompt id using url params

--- prompt-engineer/src/pages/EditPrompts.tsx
+++ prompt-engineer/src/pages/EditPrompts.tsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
+
 function EditPrompts() {
+    const promptId = useParams().id;
+
     return (
         <>
             <p>Hello world!</p>
+            <p>{promptId}</p>
         </>
     );
 }
