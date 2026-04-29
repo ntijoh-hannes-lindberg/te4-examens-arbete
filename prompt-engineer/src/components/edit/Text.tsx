4de1879e621ba39c04663d55fb3d8776d235c994
hanneslindberg <hannes.lindberg@elev.ga.ntig.se>
2026-04-29 14:06:24 +0200

    feat: removed redundant text and title components

--- prompt-engineer/src/components/edit/Text.tsx
+++
@@ -1,9 +0,0 @@
-import { TextField } from '../form/TextField'
-
-export function Text({text, setText}) {
-    return (
-        <TextField state={text} setState={setText} label="Prompt" />
-    )
-}
-
-export default Text
--- prompt-engineer/src/components/edit/Title.tsx
+++
@@ -1,9 +0,0 @@
-import { TextField } from '../form/TextField'
-
-export function Title({title, setTitle}) {
-    return (
-        <TextField state={title} setState={setTitle} label='Title' />
-    )
-}
-
-export default Title
